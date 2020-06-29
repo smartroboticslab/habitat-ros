bd35a6ae4119189c025cf0a36e75bc08f7367521
Sotiris Papatheodorou <sotirisp@protonmail.com>
2020-06-29 17:39:48 +0300

    Fix published ground truth pose to be in the correct frame

--- habitat_ros_node.py
+++ habitat_ros_node.py
@@ -4,11 +4,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import os
-import sys
 
 import cv2
 import habitat_sim as hs
 import numpy as np
+import quaternion
 import rospkg
 import rospy
 
@@ -113,33 +113,52 @@ def init_habitat(config: Dict) -> hs.Simulator:
 
 def observation_to_posemsg(obs: hs.sensor.Observation) -> PoseStamped:
     """Convert the agent pose in the observation to a PoseStamped message"""
+    position = obs['T_WB'][0:3, 3]
+    orientation = quaternion.from_rotation_matrix(obs['T_WB'][0:3, 0:3])
     p = PoseStamped()
-    p.header.frame_id = 'world'
+    p.header.frame_id = 'map'
     # Return the current ROS time since the habitat simulator does not provide
     # one
     p.header.stamp = rospy.get_rostime()
-    p.pose.position.x = obs['t_WC'][0]
-    p.pose.position.y = obs['t_WC'][1]
-    p.pose.position.z = obs['t_WC'][2]
-    p.pose.orientation.x = obs['q_WC'].x
-    p.pose.orientation.y = obs['q_WC'].y
-    p.pose.orientation.z = obs['q_WC'].z
-    p.pose.orientation.w = obs['q_WC'].w
+    p.pose.position.x = position[0]
+    p.pose.position.y = position[1]
+    p.pose.position.z = position[2]
+    p.pose.orientation.x = orientation.x
+    p.pose.orientation.y = orientation.y
+    p.pose.orientation.z = orientation.z
+    p.pose.orientation.w = orientation.w
     return p
+
 
 
 def render(sim: hs.Simulator) -> hs.sensor.Observation:
     # Just spin in a circle
     # TODO move in a more meaningful way
     observation = sim.step("turn_right")
+    observation = sim.step("move_forward")
+    observation = sim.step("move_forward")
     # Change from RGBA to RGB
     observation['rgb'] = observation['rgb'][..., 0:3]
     # TODO process depth
     # TODO process semantics
-    # Return the agent ground truth position (t_WC) and orientation (q_WC)
-    # TODO convert to z-forward, x-right
-    observation['t_WC'] = sim.get_agent(0).get_state().position
-    observation['q_WC'] = sim.get_agent(0).get_state().rotation
+    # Get the camera ground truth pose (T_HC) in the habitat frame from the
+    # position and orientation
+    t_HC = sim.get_agent(0).get_state().position
+    q_HC = sim.get_agent(0).get_state().rotation
+    T_HC = np.identity(4)
+    T_HC[0:3, 0:3] = quaternion.as_rotation_matrix(q_HC)
+    T_HC[0:3, 3] = t_HC
+    # Change from the habitat frame (y-up) to the world frame (z-up)
+    T_WH = np.identity(4)
+    T_WH[0:3, 0:3] = quaternion.as_rotation_matrix(hs.utils.common.quat_from_two_vectors(
+            hs.geo.GRAVITY, np.array([0.0, 0.0, -1.0])))
+    T_WC = np.matmul(T_WH, T_HC)
+    # Change from the camera frame (-z-forward, y-up) to the ROS body frame
+    # (x-forward, z-up)
+    T_CB = np.array([(0.0, -1.0, 0.0, 0.0), (0.0, 0.0, 1.0, 0.0),
+            (-1.0, 0.0, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0)])
+    T_WB = np.matmul(T_WC, T_CB)
+    observation['T_WB'] = T_WB
     # TODO Return the simulation timestamp. sim.get_world_time() returns 0
     return observation
 
