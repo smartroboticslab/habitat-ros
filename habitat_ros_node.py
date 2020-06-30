66e95d517ac1405652def6eed23fad436efa90f4
Sotiris Papatheodorou <sotirisp@protonmail.com>
2020-06-30 20:19:51 +0300

    Add semantics publisher

--- habitat_ros_node.py
+++ habitat_ros_node.py
@@ -15,7 +15,60 @@ import rospy
 from cv_bridge import CvBridge
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import Image
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
+
+
+
+# Convert Matterport3D class ID to class name
+class_id_to_name = {
+    0: 'void',
+    1: 'wall',
+    2: 'floor',
+    3: 'chair',
+    4: 'door',
+    5: 'table',
+    6: 'picture',
+    7: 'cabinet',
+    8: 'cushion',
+    9: 'window',
+    10: 'sofa',
+    11: 'bed',
+    12: 'curtain',
+    13: 'chest_of_drawers',
+    14: 'plant',
+    15: 'sink',
+    16: 'stairs',
+    17: 'ceiling',
+    18: 'toilet',
+    19: 'stool',
+    20: 'towel',
+    21: 'mirror',
+    22: 'tv_monitor',
+    23: 'shower',
+    24: 'column',
+    25: 'bathtub',
+    26: 'counter',
+    27: 'fireplace',
+    28: 'lighting',
+    29: 'beam',
+    30: 'railing',
+    31: 'shelving',
+    32: 'blinds',
+    33: 'gym_equipment',
+    34: 'seating',
+    35: 'board_panel',
+    36: 'furniture',
+    37: 'appliances',
+    38: 'clothes',
+    39: 'objects',
+    40: 'misc',
+    41: 'unlabeled',
+}
+
+
+
+_bridge = CvBridge()
+
 
 
 def print_node_config(config: Dict) -> None:
@@ -29,13 +82,15 @@ def print_node_config(config: Dict) -> None:
 def read_node_config() -> Dict:
     """Read the node parameters, print them and return a dictionary"""
     # Available parameter names and default values
-    param_names = ['rgb_topic_name', 'depth_topic_name', 'semantics_topic_name',
+    param_names = ['rgb_topic_name', 'depth_topic_name',
+        'semantic_class_topic_name', 'semantic_instance_topic_name',
         'habitat_pose_topic_name', 'external_pose_topic_name',
         'external_pose_topic_type', 'width', 'height', 'scene_file',
         'enable_external_pose']
     param_default_values = ['/habitat/rgb', '/habitat/depth',
-        '/habitat/semantics', '/habitat/pose', '/habitat/ext_pose',
-        'geometry_msgs::PoseStamped', 640, 480, '', False]
+        '/habitat/semantic_class', '/habitat/semantic_instance',
+        '/habitat/pose', '/habitat/ext_pose', 'geometry_msgs::PoseStamped',
+        640, 480, '', False]
 
     # Read the parameters
     config = {}
@@ -111,16 +166,6 @@ def init_habitat(config: Dict) -> hs.Simulator:
 
 
 
-def rgb_to_msg(rgb: np.ndarray) -> Image:
-    return CvBridge().cv2_to_imgmsg(rgb, "rgb8")
-
-
-
-def depth_to_msg(depth: np.ndarray) -> Image:
-    return CvBridge().cv2_to_imgmsg(depth, "32FC1")
-
-
-
 def pose_to_msg(T_WB: np.ndarray) -> PoseStamped:
     """Convert the agent pose in the observation to a PoseStamped message"""
     position = T_WB[0:3, 3]
@@ -141,15 +186,46 @@ def pose_to_msg(T_WB: np.ndarray) -> PoseStamped:
 
 
 
-def render(sim: hs.Simulator) -> hs.sensor.Observation:
+def rgb_to_msg(rgb: np.ndarray) -> Image:
+    return _bridge.cv2_to_imgmsg(rgb, "rgb8")
+
+
+
+def depth_to_msg(depth: np.ndarray) -> Image:
+    return _bridge.cv2_to_imgmsg(depth, "32FC1")
+
+
+
+def sem_instances_to_msg(sem_instances: np.ndarray) -> Image:
+    return _bridge.cv2_to_imgmsg(sem_instances.astype(np.uint16), "16UC1")
+
+
+
+def sem_classes_to_msg(sem_classes: np.ndarray) -> Image:
+    return _bridge.cv2_to_imgmsg(sem_classes.astype(np.uint8), "8UC1")
+
+
+
+def render(sim: hs.Simulator, instance_to_class: Dict[int, int]) -> hs.sensor.Observation:
     # Just spin in a circle
     # TODO move in a more meaningful way
     observation = sim.step("turn_right")
     observation = sim.step("move_forward")
     observation = sim.step("move_forward")
+
     # Change from RGBA to RGB
     observation['rgb'] = observation['rgb'][..., 0:3]
-    # TODO process semantics
+
+    # Assuming the scene has no more than 65534 objects
+    observation['sem_instances'] = np.clip(observation['semantics'].astype(np.uint32), 0, 65535)
+    del observation['semantics']
+    # Convert instance IDs to class IDs
+    observation['sem_classes'] = np.zeros(observation['sem_instances'].shape, dtype=np.uint8)
+    for y in range(observation['sem_classes'].shape[1]):
+        for x in range(observation['sem_classes'].shape[0]):
+            observation['sem_classes'][x, y] = instance_to_class[observation['sem_instances'][x, y]]
+    observation['sem_classes'] = observation['sem_instances'].astype(np.uint8)
+
     # Get the camera ground truth pose (T_HC) in the habitat frame from the
     # position and orientation
     t_HC = sim.get_agent(0).get_state().position
@@ -172,6 +248,14 @@ def render(sim: hs.Simulator) -> hs.sensor.Observation:
 
 
 
+def generate_instance_to_class_map(objects: List[hs.scene.SemanticObject]) -> Dict[int, int]:
+    map = {}
+    for instance_id in range(len(objects)):
+        map[instance_id] = objects[instance_id].category.index()
+    return map
+
+
+
 def init_node() -> Tuple[Dict, hs.Simulator]:
     """Initialize the ROS node and Habitat simulator"""
     rospy.init_node('habitat_ros')
@@ -183,21 +267,32 @@ def init_node() -> Tuple[Dict, hs.Simulator]:
 
 def run_publisher_node(config: Dict, sim: hs.Simulator) -> None:
     """Start the ROS publisher node"""
+    # Setup the instance/class conversion map
+    instance_to_class = generate_instance_to_class_map(sim.semantic_scene.objects)
+
     # Setup the image and pose publishers
+    pose_pub = rospy.Publisher(config['habitat_pose_topic_name'], PoseStamped, queue_size=10)
     rgb_pub = rospy.Publisher(config['rgb_topic_name'], Image, queue_size=10)
     depth_pub = rospy.Publisher(config['depth_topic_name'], Image, queue_size=10)
-    pose_pub = rospy.Publisher(config['habitat_pose_topic_name'], PoseStamped, queue_size=10)
-    # TODO setup semantics publisher
+    if instance_to_class:
+        # Only publish semantics if the scene contains semantics
+        sem_class_pub = rospy.Publisher(config['semantic_class_topic_name'], Image, queue_size=10)
+        sem_instance_pub = rospy.Publisher(config['semantic_instance_topic_name'], Image, queue_size=10)
+    else:
+        rospy.logwarn('The scene contains no semantics')
+
     # Main publishing loop
     # TODO decouple movement rate from camera framerate, read both from config
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        observation = render(sim)
+        observation = render(sim, instance_to_class)
+        pose_pub.publish(pose_to_msg(observation['T_WB']))
         rgb_pub.publish(rgb_to_msg(observation['rgb']))
         depth_pub.publish(depth_to_msg(observation['depth']))
-        pose_pub.publish(pose_to_msg(observation['T_WB']))
-        # TODO publish semantics
-        # TODO publish rgb/depth/semantics visualisations. Use $TOPICNAME_render
+        if instance_to_class:
+            sem_class_pub.publish(sem_classes_to_msg(observation['sem_classes']))
+            sem_instance_pub.publish(sem_instances_to_msg(observation['sem_instances']))
+        # TODO publish semantics visualisation. Use $TOPICNAME_render
         rate.sleep()
 
 
