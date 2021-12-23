9f56213a5206240c65e50f73ebe7df0de5aedec8
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2021-12-23 10:57:01 +0200

    Allow controlling roll and pitch in teleop

--- scripts/teleop.py
+++ scripts/teleop.py
@@ -28,28 +28,40 @@ _path_output_topic_type = Path
 
 
 class Movement:
-    _fb_step = 0.25 # metres, forward/backward movement step
-    _lr_step = 0.25 # metres, left/right movement step
-    _ud_step = 0.25 # metres, up/down movement step
-    _ro_step = 5 # degrees, left/right rotation step
+    # All movement happens with respect to the body frame.
+    _x_step = 0.25 # metres, x-axis movement step
+    _y_step = 0.25 # metres, y-axis movement step
+    _z_step = 0.25 # metres, z-axis movement step
+    _roll_step = 5 # degrees, roll rotation step
+    _pitch_step = 5 # degrees, pitch rotation step
+    _yaw_step = 5 # degrees, yaw rotation step
 
-    def __init__(self, forward: int=0, left: int=0, up: int=0, rotate_left: int=0) -> None:
-        self._fb = forward
-        self._lr = left
-        self._ud = up
-        self._ro = rotate_left
+    def __init__(self, x: int=0, y: int=0, z: int=0,
+            roll: int=0, pitch: int=0, yaw: int=0) -> None:
+        self._x = x
+        self._y = y
+        self._z = z
+        self._roll = roll
+        self._pitch = pitch
+        self._yaw = yaw
 
-    def fb(self) -> float:
-        return self._fb * Movement._fb_step
+    def x(self) -> float:
+        return self._x * Movement._x_step
 
-    def lr(self) -> float:
-        return self._lr * Movement._lr_step
+    def y(self) -> float:
+        return self._y * Movement._y_step
 
-    def ud(self) -> float:
-        return self._ud * Movement._ud_step
+    def z(self) -> float:
+        return self._z * Movement._z_step
 
-    def ro(self) -> float:
-        return math.radians(self._ro * Movement._ro_step)
+    def roll(self) -> float:
+        return math.radians(self._roll * Movement._roll_step)
+
+    def pitch(self) -> float:
+        return math.radians(self._pitch * Movement._pitch_step)
+
+    def yaw(self) -> float:
+        return math.radians(self._yaw * Movement._yaw_step)
 
 
 
@@ -101,31 +113,34 @@ def update_pose(p: PoseStamped, m: Movement) -> PoseStamped:
     new_p = PoseStamped()
     new_p.header.stamp = rospy.get_rostime()
     new_p.header.frame_id = p.header.frame_id
-    new_p.pose.position = p.pose.position
+    # T_HB, current pose
+    T_HB = np.identity(4)
+    T_HB[0, 3] = p.pose.position.x
+    T_HB[1, 3] = p.pose.position.y
+    T_HB[2, 3] = p.pose.position.z
     q_current = quaternion.quaternion(p.pose.orientation.w,
             p.pose.orientation.x, p.pose.orientation.y, p.pose.orientation.z)
-    R_current = quaternion.as_rotation_matrix(q_current)
-    theta_current = math.atan2(R_current[1,0], R_current[0,0])
-    # Forwards/backwards movement
-    new_p.pose.position.x += m.fb() * math.cos(theta_current)
-    new_p.pose.position.y += m.fb() * math.sin(theta_current)
-    # Left/right movement
-    new_p.pose.position.x += m.lr() * math.cos(theta_current + math.pi/2)
-    new_p.pose.position.y += m.lr() * math.sin(theta_current + math.pi/2)
-    # Up/down movement
-    new_p.pose.position.z = p.pose.position.z + m.ud()
-    # Left/right rotation
-    theta_new = theta_current + m.ro()
-    R = np.eye(3)
-    R[0,0] = math.cos(theta_new)
-    R[0,1] = -math.sin(theta_new)
-    R[1,0] = math.sin(theta_new)
-    R[1,1] = math.cos(theta_new)
-    q_new = quaternion.as_float_array(quaternion.from_rotation_matrix(R))
-    new_p.pose.orientation.x = q_new[1]
-    new_p.pose.orientation.y = q_new[2]
-    new_p.pose.orientation.z = q_new[3]
-    new_p.pose.orientation.w = q_new[0]
+    T_HB[0:3, 0:3] = quaternion.as_rotation_matrix(q_current)
+    # T_BBnew, move with respect to the body frame
+    T_BBnew = np.identity(4)
+    T_BBnew[0, 3] += m.x()
+    T_BBnew[1, 3] += m.y()
+    T_BBnew[2, 3] += m.z()
+    q_yaw = quaternion.quaternion(math.cos(m.yaw() / 2), 0, 0, math.sin(m.yaw() / 2))
+    q_pitch = quaternion.quaternion(math.cos(m.pitch() / 2), 0, math.sin(m.pitch() / 2), 0)
+    q_roll = quaternion.quaternion(math.cos(m.roll() / 2), math.sin(m.roll() / 2), 0, 0)
+    q_new = q_yaw * q_pitch * q_roll
+    T_BBnew[0:3, 0:3] = quaternion.as_rotation_matrix(q_new)
+    # T_HBnew, new pose
+    T_HBnew = T_HB @ T_BBnew
+    q = quaternion.from_rotation_matrix(T_HBnew[0:3, 0:3])
+    new_p.pose.position.x = T_HBnew[0, 3]
+    new_p.pose.position.y = T_HBnew[1, 3]
+    new_p.pose.position.z = T_HBnew[2, 3]
+    new_p.pose.orientation.x = q.x
+    new_p.pose.orientation.y = q.y
+    new_p.pose.orientation.z = q.z
+    new_p.pose.orientation.w = q.w
     return new_p
 
 
@@ -150,28 +165,40 @@ def wait_for_key(window) -> Tuple[Movement, bool]:
                 quit = True
                 break
             elif key == "w":
-                m = Movement(forward=1)
+                m = Movement(x=1)
                 break
             elif key == "s":
-                m = Movement(forward=-1)
+                m = Movement(x=-1)
                 break
             elif key == "a":
-                m = Movement(left=1)
+                m = Movement(y=1)
                 break
             elif key == "d":
-                m = Movement(left=-1)
+                m = Movement(y=-1)
                 break
             elif key == " ":
-                m = Movement(up=1)
+                m = Movement(z=1)
                 break
             elif key == "c":
-                m = Movement(up=-1)
+                m = Movement(z=-1)
                 break
             elif key == "q":
-                m = Movement(rotate_left=1)
+                m = Movement(yaw=1)
                 break
             elif key == "e":
-                m = Movement(rotate_left=-1)
+                m = Movement(yaw=-1)
+                break
+            elif key == "f":
+                m = Movement(pitch=1)
+                break
+            elif key == "r":
+                m = Movement(pitch=-1)
+                break
+            elif key == "x":
+                m = Movement(roll=1)
+                break
+            elif key == "z":
+                m = Movement(roll=-1)
                 break
             time.sleep(0.05)
     finally:
@@ -188,13 +215,15 @@ def print_waiting_for_pose(window) -> None:
 
 
 def print_help(window) -> None:
-    window.addstr(0, 0, "Position:")
-    window.addstr(2, 0, "Orientation (w,x,y,z):")
-    window.addstr(5, 0, "w/s       forwards/backwards")
-    window.addstr(6, 0, "a/d       left/right")
-    window.addstr(7, 0, "space/c   up/down")
-    window.addstr(8, 0, "q/e       rotate left/right")
-    window.addstr(9, 0, "Q         quit")
+    window.addstr(0,  0, "Position:")
+    window.addstr(2,  0, "Orientation (w,x,y,z):")
+    window.addstr(5,  0, "w/s       forwards/backwards")
+    window.addstr(6,  0, "a/d       left/right")
+    window.addstr(7,  0, "space/c   up/down")
+    window.addstr(8,  0, "q/e       yaw left/right")
+    window.addstr(9,  0, "r/f       pitch up/down")
+    window.addstr(10, 0, "z/x       roll CCW/CW")
+    window.addstr(11, 0, "Q         quit")
 
 
 
